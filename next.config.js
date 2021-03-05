ee00532969aa8dd10a332f3cd1a5c29ca63c70c9
Noah Kogler <noah.kogler@gmail.com>
2021-03-05 18:01:04 +0100

    fixed paths

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const { i18n } = require('./next-i18next.config')
 const withImages = require('next-images')
 
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = withImages({
-  assetPrefix: isProd ? 'https://birgittas-friends-next.vercel.app' : '',
   i18n,
 })
