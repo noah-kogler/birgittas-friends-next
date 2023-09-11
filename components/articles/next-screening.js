4ca36064ed1ef8fa4b4761161c67fcf492cfb6e8
Noah Kogler <noah.kogler@gmail.com>
2023-09-11 17:36:12 +0200

    add next screening in Braunau

---
+++ components/articles/next-screening.js
@@ -0,0 +1,19 @@
+import { useCommonTranslation } from "../../tools/i18n";
+import { Trans } from "react-i18next";
+
+export default function NextScreening() {
+  const { t } = useCommonTranslation();
+
+  return (
+    <article id="next-screening" className="mt-20">
+      <h3 className="font-header font-bold text-2.5xl">{t('next_screening_title')}</h3>
+      <p className="mt-6">
+        <Trans i18nKey="next_screening_text">
+          <span className="font-bold" />
+          <span className="font-bold" />
+          <a href="https://www.dieselkino.at/index.php?cine=09&id=102" className="font-bold" />
+        </Trans>
+      </p>
+    </article>
+  );
+}
